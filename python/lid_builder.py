c0a8358d56cd9af4947bd2b0af8642b2eb4ea3c8
Igor Sitdikov <igor.sitdikov@codex-soft.com>
2021-04-03 00:00:33 +0300

    fix path;

--- python/lid_builder.py
+++ python/lid_builder.py
@@ -3,7 +3,7 @@
 import os
 from cffi import FFI
 
-lid_root=os.environ.get("LID_SOURCE", ".")
+lid_root=os.environ.get("LID_SOURCE", "..")
 cpp_command = "cpp " + lid_root + "/native/lid_api.h"
 
 ffibuilder = FFI()
